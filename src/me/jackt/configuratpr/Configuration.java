a830b499b7b88c141bf90a3fb764cb1e4816a5cf
Jack Townsend <jackmaverick1@gmail.com>
2014-03-02 00:10:10 -0500

    I wonder what's in this file...

--- src/me/jackt/configuratpr/Configuration.java
+++ src/me/jackt/configuratpr/Configuration.java
@@ -1,16 +1,28 @@
 package me.jackt.configuratpr;
 
+import me.jackt.configuratpr.me.jackt.configurator.providers.DefaultProvider;
+
+import java.util.Set;
+
 /**
  * User: Jack Date: 3/1/14 Time: 11:34 PM
  */
-public class Configuration {
+public abstract class Configuration {
 
-    private ConfigurationProvider normalProvider;
+    protected ConfigurationProvider normalProvider;
 
-    private static final ConfigurationProvider defaultProvider;
+    protected static final ConfigurationProvider defaultProvider;
 
     static {
+        defaultProvider = new DefaultProvider();
     }
+
+    /**
+     * Fill the normal provider with whatever it likes, based on the Constructors it defines.
+     */
+    protected abstract void fillNormal();
+
+
 
 
 }
